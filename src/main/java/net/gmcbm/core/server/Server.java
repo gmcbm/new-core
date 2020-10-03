0a5c07a5127d030e27d8090284b890b766b125c4
Ursin Filli <git@ursinn.dev>
2020-10-03 19:04:33 +0200

    add Server

---
+++ src/main/java/net/gmcbm/core/server/Server.java
@@ -0,0 +1,63 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Global Minecraft Ban Manager
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.gmcbm.core.server;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * @author Ursin Filli
+ * @version 1.0
+ * @since 1.0
+ */
+public class Server implements IServer {
+
+    private final UUID serverId;
+    private final String serverName;
+    private final ServerOwner serverOwner;
+
+    public Server(@NotNull UUID serverId, @NotNull String serverName, @NotNull ServerOwner serverOwner) {
+        this.serverId = Objects.requireNonNull(serverId);
+        this.serverName = Objects.requireNonNull(serverName);
+        this.serverOwner = Objects.requireNonNull(serverOwner);
+    }
+
+    @Override
+    public UUID getServerId() {
+        return this.serverId;
+    }
+
+    @Override
+    public String getServerName() {
+        return this.serverName;
+    }
+
+    @Override
+    public ServerOwner getOwner() {
+        return serverOwner;
+    }
+}
