881ae7fd34a92b851039b6f3169377ace9868990
Ursin Filli <git@ursinn.dev>
2020-10-03 17:45:51 +0200

    add Ban

---
+++ src/main/java/net/gmcbm/core/ban/Ban.java
@@ -0,0 +1,76 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Global Minecraft Ban Manager
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.gmcbm.core.ban;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * @author Ursin Filli
+ * @since 1.0
+ * @version 1.0
+ */
+public class Ban {
+
+    private final UUID uuid;
+    private final String banId;
+    private final String reason;
+    private final Instant timestamp;
+    private final BanType type;
+    private final BanStorageLevel storageLevel;
+
+    public Ban(UUID uuid, String banId, String reason, Instant timestamp, BanType type, BanStorageLevel storageLevel) {
+        this.uuid = uuid;
+        this.banId = banId;
+        this.reason = reason;
+        this.timestamp = timestamp;
+        this.type = type;
+        this.storageLevel = storageLevel;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getBanId() {
+        return banId;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public BanType getType() {
+        return type;
+    }
+
+    public BanStorageLevel getStorageLevel() {
+        return storageLevel;
+    }
+}
